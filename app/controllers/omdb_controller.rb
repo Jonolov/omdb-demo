c3af2bbe03f2d14fb1c3146fe88f33ff3117ad75
Jonolov <jon.stjarnstrom@gmail.com>
2021-05-26 01:18:06 +0200

    Api calls that returns json data to react views.

--- app/controllers/omdb_controller.rb
+++ app/controllers/omdb_controller.rb
@@ -1,3 +1,34 @@
+# frozen_string_literal: true
+
+require 'uri'
+require 'net/http'
+
+# Controller that handles http requests to the omdb api
 class OmdbController < ApplicationController
   def index; end
+
+  def show_search_results
+    movies = find_movies(params[:title])
+  end
+
+  def show_movie
+    movie = get_movie(params[:id])
+  end
+
+  def find_movies(movie_title)
+    uri = URI('http://www.omdbapi.com/')
+    params = { apikey: ENV['OMDB_API_KEY'], s: movie_title } # The api key is placed in .env
+    uri.query = URI.encode_www_form(params)
+    res = Net::HTTP.get_response(uri)
+    render json: res.body
+  end
+
+  def get_movie(imdb_id)
+    logger.debug "The id: #{imdb_id}"
+    uri = URI('http://www.omdbapi.com/')
+    params = { apikey: ENV['OMDB_API_KEY'], i: imdb_id }
+    uri.query = URI.encode_www_form(params)
+    res = Net::HTTP.get_response(uri)
+    render json: res.body
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  # get 'omdb/index'
   root 'omdb#index'
+  get '/movie/:id' => 'omdb#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/search/:title' => 'omdb#show_search_results'
+  get '/search/movie/:id' => 'omdb#show_movie'
 end
