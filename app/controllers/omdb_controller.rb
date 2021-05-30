fbf75d989dc2079c93d72b62473fcb99fd2d9fb6
Jonolov <jon.stjarnstrom@gmail.com>
2021-05-30 23:56:53 +0200

    Refactored routes.

--- app/controllers/omdb_controller.rb
+++ app/controllers/omdb_controller.rb
@@ -8,27 +8,34 @@ class OmdbController < ApplicationController
   def index; end
 
   def show_search_results
-    movies = find_movies(params[:title])
+    @movies = find_movies(params[:title])
+    render json: @movies
   end
 
   def show_movie
-    movie = get_movie(params[:id])
+    @movie = get_movie(params[:id])
+    render json: @movie
   end
 
   def find_movies(movie_title)
     uri = URI('http://www.omdbapi.com/')
     params = { apikey: ENV['OMDB_API_KEY'], s: movie_title } # The api key is placed in .env
     uri.query = URI.encode_www_form(params)
-    res = Net::HTTP.get_response(uri)
-    render json: res.body
+    response = Net::HTTP.get_response(uri)
+
+    raise HttpError.new(response.code, response.message) if response.code != '200'
+
+    response.body
   end
 
   def get_movie(imdb_id)
-    logger.debug "The id: #{imdb_id}"
     uri = URI('http://www.omdbapi.com/')
     params = { apikey: ENV['OMDB_API_KEY'], i: imdb_id }
     uri.query = URI.encode_www_form(params)
     res = Net::HTTP.get_response(uri)
-    render json: res.body
+
+    raise HttpError.new(response.code, response.message) if response.code != '200'
+
+    res.body
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'omdb#index'
   get '/movie/:id' => 'omdb#index'
+  get '/movies/:title' => 'omdb#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/search/:title' => 'omdb#show_search_results'
+  get '/search/movies/:title' => 'omdb#show_search_results'
   get '/search/movie/:id' => 'omdb#show_movie'
+  match '*path', to: 'omdb#index', via: :all
 end
