32e5653b4d36b3115749dc08dc9f828febb3b8b4
Jonolov <jon.stjarnstrom@gmail.com>
2021-05-30 23:00:38 +0200

    Bugfix: several instances of material-ui/styles

---
+++ config/webpack/custom.js
@@ -0,0 +1,13 @@
+module.exports = {
+  resolve: {
+    resolve: {
+      alias: {
+        "@material-ui/styles": path.resolve(
+          appFolder,
+          "node_modules",
+          "@material-ui/styles"
+        ),
+      },
+    },
+  },
+};
